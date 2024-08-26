90622cc4df461dd345d5a23ea5c3276be0e0a5c8
lsy1307 <lsy1307@inha.edu>
2024-08-26 17:25:43 +0900

    Chore: prettier 및 기타 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
 
 function App() {
-
   return (
     <BrowserRouter>
-        <Routes>
-            <Route>
-
-            </Route>
-        </Routes>
+      <Routes>
+        <Route></Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
