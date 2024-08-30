80f83ce0794da710bd442e40775db3abbd051145
lsy1307 <lsy1307@inha.edu>
2024-08-30 11:08:50 +0900

    Feat: 작업중

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,85 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
-import MobileLogin from './MobileLogin';
-import MobileTrip from './MobileTrip';
-import MobileTripDetail from './MobileTripDetail';
-import MobilePost from './MobilePost';
-import MobilePostDetail from './MobilePostDetail';
-import MobileClip from './MobileClip';
-import MobileClipDetail from './MobileClipDetail';
-import Login from './Login';
-import CallBack from './CallBack';
-import Main from './Main';
-import Plan from './Plan';
-import PlanDetail from './PlanDetail';
-import Post from './Post';
-import PostDetail from './PostDetail';
-import MyPage from './MyPage';
-import MyPageModify from './MyPageModify';
-import AdminLogin from './AdminLogin';
-import Admin from './Admin';
-import AdminManage from './AdminManage';
-import AdminManageClip from './AdminManageClip';
-import AdminManagePost from './AdminManagePost';
-import AdminManageNotice from './AdminManageNotice';
-import AdminManageTrip from './AdminManageTrip';
+import MobileLogin from './pages/Mobile/MobileLogin';
+import MobileTrip from './pages/Mobile/MobileTrip';
+import MobileTripDetail from './pages/Mobile/MobileTripDetail';
+import MobilePost from './pages/Mobile/MobilePost';
+import MobilePostDetail from './pages/Mobile/MobilePostDetail';
+import MobileClip from './pages/Mobile/MobileClip';
+import MobileClipDetail from './pages/Mobile/MobileClipDetail';
+import Login from './pages/User/Login';
+import CallBack from './auth/CallBack';
+import Main from './pages/Main';
+import Plan from './pages/Plan/Plan';
+import PlanDetail from './pages/Plan/PlanDetail';
+import Post from './pages/Post/Post';
+import PostDetail from './pages/Post/PostDetail';
+import MyPage from './pages/User/MyPage';
+import MyPageModify from './pages/User/MyPageModify';
+import AdminLogin from './pages/Admin/AdminLogin';
+import Admin from './pages/Admin/Admin';
+import AdminManage from './pages/Admin/AdminManage';
+import AdminManageClip from './pages/Admin/AdminManageClip';
+import AdminManagePost from './pages/Admin/AdminManagePost';
+import AdminManageNotice from './pages/Admin/AdminManageNotice';
+import AdminManageTrip from './pages/Admin/AdminManageTrip';
+import { setIsMobile } from './redux/example/AuthActions';
 
 function App() {
+  const isMobile = useSelector((state) => state.auth.isMobile);
+  const dispatch = useDispatch();
   const location = useLocation();
-  const [isMobile, setIsMobile] = useState(false);
-
   useEffect(() => {
     const mobileCheck = /Mobi|Android/i.test(navigator.userAgent);
-    setIsMobile(mobileCheck);
-
+    dispatch(setIsMobile(mobileCheck));
     if (mobileCheck && !window.location.hostname.startsWith('m.')) {
       window.location.href = `${location.pathname}`;
     }
   }, [location]);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        {isMobile ? (
-          <>
-            <Route path="/login" element={<MobileLogin />} />
-            <Route path="/trip" element={<MobileTrip />} />
-            <Route path="/trip/:tripId" element={<MobileTripDetail />} />
-            <Route path="/post" element={<MobilePost />} />
-            <Route path="/post/:postId" element={<MobilePostDetail />} />
-            <Route path="/clip" element={<MobileClip />} />
-            <Route path="/clip/:clipId" element={<MobileClipDetail />} />
-          </>
-        ) : (
-          <>
-            <Route path="/login" element={<Login />} />
-            <Route path="/callback" element={<CallBack />} />
-            <Route path="/main" element={<Main />} />
-            <Route path="/plan" element={<Plan />} />
-            <Route path="/plan/:planId" element={<PlanDetail />} />
-            <Route path="/post" element={<Post />} />
-            <Route path="/post/:postId" element={<PostDetail />} />
-            <Route path="/mypage" element={<MyPage />} />
-            <Route path="/mypage/modify" element={<MyPageModify />} />
-            <Route path="/admin/login" element={<AdminLogin />} />
-            <Route path="/admin/home" element={<Admin />} />
-            <Route path="/admin/manage" element={<AdminManage />} />
-            <Route path="/admin/manage/clip" element={<AdminManageClip />} />
-            <Route path="/admin/manage/post" element={<AdminManagePost />} />
-            <Route
-              path="/admin/manage/notice"
-              element={<AdminManageNotice />}
-            />
-            <Route path="/manage/trip" element={<AdminManageTrip />} />
-          </>
-        )}
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      {isMobile ? (
+        <>
+          <Route path="/login" element={<MobileLogin />} />
+          <Route path="/callback" element={<CallBack />} />
+          <Route path="/trip" element={<MobileTrip />} />
+          <Route path="/trip/:tripId" element={<MobileTripDetail />} />
+          <Route path="/post" element={<MobilePost />} />
+          <Route path="/post/:postId" element={<MobilePostDetail />} />
+          <Route path="/clip" element={<MobileClip />} />
+          <Route path="/clip/:clipId" element={<MobileClipDetail />} />
+        </>
+      ) : (
+        <>
+          <Route path="/login" element={<Login />} />
+          <Route path="/callback" element={<CallBack />} />
+          <Route path="/main" element={<Main />} />
+          <Route path="/plan" element={<Plan />} />
+          <Route path="/plan/:planId" element={<PlanDetail />} />
+          <Route path="/post" element={<Post />} />
+          <Route path="/post/:postId" element={<PostDetail />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/mypage/modify" element={<MyPageModify />} />
+          <Route path="/admin/login" element={<AdminLogin />} />
+          <Route path="/admin/home" element={<Admin />} />
+          <Route path="/admin/manage" element={<AdminManage />} />
+          <Route path="/admin/manage/clip" element={<AdminManageClip />} />
+          <Route path="/admin/manage/post" element={<AdminManagePost />} />
+          <Route path="/admin/manage/notice" element={<AdminManageNotice />} />
+          <Route path="/manage/trip" element={<AdminManageTrip />} />
+        </>
+      )}
+    </Routes>
   );
 }
 
-export default App;
+export default function MainApp() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
