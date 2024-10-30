d04189334ce7de97db4d51ed066decfa19c2d961
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2024-10-30 10:07:36 +0900

    사용자 정보 수정 화면 구현(프로필 사진 수정 기능 보완 예정)

---
+++ src/api/uploadProfile.js
@@ -0,0 +1,19 @@
+import { PostAxiosInstance } from '../axios/AxiosMethod';
+
+export const uploadProfileImage = async (file) => {
+  try {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    const response = await PostAxiosInstance().post('/user/profile', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Error uploading profile image:', error);
+    throw error;
+  }
+};
